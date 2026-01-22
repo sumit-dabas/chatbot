2f612f09e22492e7b68af53db1168c2b7bf25dbd
sumit-dabas <sumitdabas050@gmail.com>
2026-01-22 21:53:25 +0530

    [First Commit of chatbot with short memory capability]

---
+++ streamlit_frontend.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from langgraph_backend import chatbot
+from langchain_core.messages import HumanMessage
+
+
+#st.session_state -> DICTIONARY hi hota but jab enter press krte tw iske andr ki cheezein erase ni hoti.. vo as it is rehti hein (until-unless jabtk page ko hard-refresh ni krte)
+CONFIG = {'configurable' : {'thread_id': 'thread-1'}}
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+#loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+user_input = st.chat_input('Type here')
+
+if user_input:
+
+    #first add the message to message_history- CURRENT USER MESSAGE:
+    st.session_state['message_history'].append({'role':'user', 'content':user_input})
+
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+    ai_message = response['messages'][-1].content
+    #first add the message to message_history- CURRENT ASSISTANT MESSAGE:
+    st.session_state['message_history'].append({'role':'assistant', 'content':ai_message})
+    with st.chat_message('assistant'):
+        st.text(ai_message)
